78e8d1d5cd859f0d113f248744fc77223689b993
Vaibhav Shinde <vaibhavshinde1642@gmail.com>
2021-11-19 09:37:03 +0530

    initial commit

---
+++ src/main/java/com/vaibhavshindetech/S37ContextPathAppApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhavshindetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S37ContextPathAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S37ContextPathAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaibhavshindetech/S37ContextPathAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavshindetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S37ContextPathAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
